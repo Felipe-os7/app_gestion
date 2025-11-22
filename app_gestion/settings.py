258afde6c0587275a6ca99eab699271cdcfc24ef
felipechamorroretamal@gmail.com <felipechamorroretamal@gmail.com>
2025-11-22 00:03:32 -0300

    setting mod

--- app_gestion/settings.py
+++ app_gestion/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-%n^_6vvogjp9mb(_r(kp+aq^_=ni!bq(jjm-6cna)8b7n=)skz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["app-gestion-6vnf.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    'app-gestion-6vnf.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
